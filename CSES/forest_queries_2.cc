78637e307cc676239aac98f5287a0f3dc65f50da
d_rey <contactodr94@gmail.com>
2020-02-03 03:14:09 -0300

    Comments were deleted.

--- CSES/forest_queries_2.cc
+++ CSES/forest_queries_2.cc
@@ -1,18 +1,17 @@
 // https://cses.fi/problemset/task/1739
 #include <iostream>
-#define ll long long
 #define M 200000
 #define N 1000
 #define MAX_POW_2 1024
 using namespace std;
+using ll = long long;
+
 static pair<ll,ll> query[M];
 static ll m[N][N];
 static ll* main_st[2*MAX_POW_2];
 
-// Create dummy segment tree for filling the main_st;
 ll* create_dummy_leaf(ll size) {
   ll* st = (ll*)calloc(2*size,sizeof(ll));
-  // Put column elements as leaves of tree
   for (ll i = 0; i < size; i++) {
     st[size+i] = 0;
   }
@@ -26,17 +25,14 @@ ll* create_dummy_leaf(ll size) {
   return st;
 }
 
-// Create the segment trees for the column in the matrix
 ll* create_st_leaf(ll j, ll size, ll original_size) {
   ll* st = (ll*)calloc(2*size,sizeof(ll));
-  // Put column elements as leaves of tree
   for (ll i = 0; i < original_size; i++) {
     st[size+i] = m[i][j];
   }
   for (ll i = original_size; i < size; i++) {
     st[size+i] = 0;
   }
-  // Get next levels 
   size /= 2;
   while (size) {
     for (ll i = 0; i < size; i++) {
@@ -49,11 +45,9 @@ ll* create_st_leaf(ll j, ll size, ll original_size) {
 
 ll* compress_level(ll left, ll right, ll size) {
   ll* st = (ll*)calloc(2*size, sizeof(ll));
-  // Compress left and right columns by rows operations 
   for (ll i = 0; i < size; i++) {
     st[size+i] = main_st[left][size+i] + main_st[right][size+i];
   }
-  // Get next levels
   size /= 2;
   while (size) {
     for (ll i = 0; i < size; i++) {
@@ -64,24 +58,19 @@ ll* compress_level(ll left, ll right, ll size) {
   return st;
 }
 
-// Destroy segment trees
 void destroy_main_st(ll size) {
   for (ll i = 0; i < 2*size; i++) {
     free(main_st[i]);
   }
 }
 
-// Fill the main_st with segment trees in the nodes for the columns
 void init_main_st(ll size, ll original_size) {
-  // Create leafs for the tree
   for (ll j = 0; j < original_size; j++) {
     main_st[size+j] = create_st_leaf(j, size, original_size);
   }
   for (ll j = original_size; j < size; j++) {
     main_st[size+j] = create_dummy_leaf(size);
   }
-  // main_st[size+j] = Segment Tree for column j
-  // Now, freely create interior nodes
   ll level = size/2;
   while (level) {
     for (ll i = 0; i < level; i++) {
@@ -94,16 +83,14 @@ void init_main_st(ll size, ll original_size) {
 void add(ll st[], ll index, ll size, ll x) {
   index += size;
   st[index] += x;
-  // Add to parents
+
   for (index /= 2; index >= 1; index /= 2) {
     st[index] = st[2*index] + st[2*index+1];
   }
 }
 
 void add_at(ll i, ll j, ll size, ll x) {
-  // Add to corresponding leaf
   add(main_st[size+j], i, size, x);
-  // Add to corresponding parents of leaf
   ll parent = (size+j)/2;
   while (parent) {
     add(main_st[parent], i, size, x);
@@ -146,38 +133,6 @@ ll sum_coord(ll y1,ll x1, ll y2, ll x2, ll size) {
   return s;
 }
 
-#ifdef DEBUG
-void print_st(ll st[], ll size) {
-  for (ll i = 0; i < size; i++) {
-    cout << st[size+i] << " ";
-  }
-  cout << "\n";
-  size /= 2;
-  while (size) {
-    for (ll i = 0; i < size; i++) {
-      cout << st[size+i] << " ";
-    }
-    cout << "\n";
-    size /= 2;
-  }
-}
-
-void print_main_st(ll size) {
-  cout << "Printing leaves of main_st\n";
-  for (ll i = 0; i < size; i++) {
-    print_st(main_st[size+i], size);
-  }
-  ll level = size/2;
-  while (level) {
-    cout << "Printing new level with " << level << " elements\n";
-    for (ll i = 0; i < level; i++) {
-      print_st(main_st[level+i],size);
-    }
-    level /= 2;    
-  }
-}
-#endif
-
 int main() {
   ll n, q, full_len = 0; char f;
   ll a, b, c, d, e;
@@ -201,14 +156,6 @@ int main() {
   } else {
     init_main_st(n,n);
   }
-  #ifdef DEBUG
-  cout << "Printing main_st\n";
-  if (full_len) {
-    print_main_st(full_len);
-  } else {
-    print_main_st(n);
-  }
-  #endif
   for (ll i = 0; i < q; i++) {
     cin >> a;
     if (a==1) {
@@ -222,14 +169,6 @@ int main() {
         m[b-1][c-1] = (m[b-1][c-1]?0:1);
         query[i] = {a, 0};
       }
-      #ifdef DEBUG
-      cout << "Printing main_st after update\n";
-      if (full_len) {
-        print_main_st(full_len);
-      } else {
-        print_main_st(n);
-      }
-      #endif
     } else {
       cin >> b >> c >> d >> e;
       if (full_len) {
