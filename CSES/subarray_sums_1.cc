71300903139d701e92f2b658bba83161a15faf03
reydc <54533285+reydc@users.noreply.github.com>
2020-08-05 12:42:30 -0300

    Added new file.

--- CSES/subarray_sums_1.cc
+++ CSES/subarray_sums_1.cc
@@ -3,24 +3,21 @@
 #include <map>
 using namespace std;
 using ll = long long;
-static int a[200000];
+static ll a[200000];
+
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  ll n, x, s, c = 0;
-  map<int,int> x_diff;
+  ll n, x, s = 0, c = 0;
+  map<ll,ll> x_diff;
   cin >> n >> x;
-  cin >> a[0];
-  s = a[0];
-  x_diff[s]++;
-  c += s==x;
-  for (ll i = 1; i < n; i++) {
-    cin >> a[i];
+  for (auto i = 0; i < n; i++) cin >> a[i];
+  for (auto i = 0; i < n; i++) {
     s += a[i];
-    c += s==x;
-    if (s > x && x_diff.find(s-x) != x_diff.end()) {
-      c += x_diff[s-x];
+    if (s > x && x_diff.find(s - x) != x_diff.end()) {
+      c += x_diff[s - x];
     }
+    if (s == x) c++;
     x_diff[s]++;
   }
   cout << c << "\n";
